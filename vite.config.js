85f172c8995ac8d152f801af84b174b6b18bcd56
Nikhileshwar Adam <nikhileshwar41@gmail.com>
2025-10-28 19:13:47 +0530

    Fix base path and CMS configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/',
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
 })
